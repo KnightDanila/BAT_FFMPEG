30ed1c0f6543407bb782eeccab6a646230632cfc
KnightDanila <KnightDanila@i.ua>
2018-12-20 13:00:35 +0300

    Audio Gain add :bird:

---
+++ Audio-AudioGain.bat
@@ -0,0 +1,37 @@
+https://stackoverflow.com/questions/3255674/convert-audio-files-to-mp3-using-ffmpeg
+https://superuser.com/questions/332347/how-can-i-convert-mp4-video-to-mp3-audio-with-ffmpeg
+
+@echo off
+echo %CD%
+echo %~dp0
+cd /d %~dp0
+
+REM For first file
+REM %1
+REM For multiple files
+echo %*
+
+set COUNTER=0
+for %%x in (%*) do (
+	echo %%x
+	set /A COUNTER+=1
+)
+echo COUNTER = %COUNTER%
+
+if %COUNTER% GTR 0 (
+	if %COUNTER% EQU 1 (
+		echo _____GET ONE FILE_____
+	) else (
+		echo _____GET MULTI FILES: %COUNTER% files_____
+	)
+	for %%x in (%*) do (
+REM //////////////////// MAIN \\\\\\\\\\\\\\\\\\\\\\\\\
+		REM https://trac.ffmpeg.org/wiki/AudioVolume
+		ffmpeg -i %%x -filter:a "volume=1.5" -ar 44100 -ac 2 -q:a 0 -map_metadata 0 -id3v2_version 3 "%%~nx_output_GAIN%%~xx"
+
+	)
+) else (
+	echo _____GET NO ONE FILES_____
+)
+
+pause
