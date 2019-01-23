96aa9d7f7d2288d7d53aa5242254fbaf526c104c
KnightDanila <KnightDanila@i.ua>
2019-01-23 14:47:46 +0300

    UpDates - youtube-dl :bird:

--- youtube-dl-Video-1080pOrMaxSize.bat
+++ youtube-dl-Video-1080pOrMaxSize.bat
@@ -1,5 +1,12 @@
 REM youtube-dl.exe --config-location youtube-dl.conf URL
 
+
+REM 	USE "@setlocal EDE (EnableDelayedExpansion )" -
+REM	Because I cannot use "SET /p" - IN LOOP, 
+REM 	Cannot use & Ampersand symbol in URL (I need it for list)
+@setlocal EnableDelayedExpansion 
+
+
 @echo off
 echo %CD%
 echo %~dp0
@@ -7,11 +14,11 @@ cd /d %~dp0
 
 ECHO. & ECHO ******************************************************
 SET /p URL="Paste URL: "
-ECHO URL=%URL%;
+ECHO URL=!URL!;
 ECHO ****************************************************** & ECHO.
 
 REM _________________MAIN_____________________
-	youtube-dl.exe --config-location youtube-dl-Video.conf %URL%
+	youtube-dl.exe --config-location youtube-dl-Video-1080pOrMaxSize.conf !URL!
 REM _______________END_MAIN___________________
 
 ECHO.
---
+++ youtube-dl-Video-MaxSize.bat
@@ -0,0 +1,30 @@
+REM youtube-dl.exe --config-location youtube-dl.conf URL
+
+
+REM 	USE "@setlocal EDE (EnableDelayedExpansion )" -
+REM	Because I cannot use "SET /p" - IN LOOP, 
+REM 	Cannot use & Ampersand symbol in URL (I need it for list)
+@setlocal EnableDelayedExpansion 
+
+
+@echo off
+echo %CD%
+echo %~dp0
+cd /d %~dp0
+
+ECHO. & ECHO ******************************************************
+SET /p URL="Paste URL: "
+ECHO URL=!URL!;
+ECHO ****************************************************** & ECHO.
+
+REM _________________MAIN_____________________
+	youtube-dl.exe --config-location youtube-dl-Video-MaxSize.conf !URL!
+REM _______________END_MAIN___________________
+
+ECHO.
+ECHO ******************************************************
+ECHO *        Downloads Are Ready :) Be Happy :)          *
+ECHO ******************************************************
+ECHO.
+
+pause
