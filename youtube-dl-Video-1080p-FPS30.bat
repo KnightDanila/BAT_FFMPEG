368f1b9352c6372907682ffbef495205ce8565d5
KnightDanila <KnightDanila@i.ua>
2019-01-24 16:18:09 +0300

    UpDates - config and new parametrs :bird:

---
+++ youtube-dl-Video-1080p-FPS30.bat
@@ -0,0 +1,34 @@
+@echo off
+REM youtube-dl.exe --config-location youtube-dl.conf URL
+
+
+REM 	USE "@setlocal EDE (EnableDelayedExpansion )" -
+REM	Because I cannot use "SET /p" - IN LOOP, 
+REM 	Cannot use & Ampersand symbol in URL (I need it for list)
+@setlocal EnableDelayedExpansion 
+
+
+echo %CD%
+echo %~dp0
+cd /d %~dp0
+ECHO. & ECHO ******* BAT FILE ******* 
+ECHO %~nx0 
+ECHO ************************ & ECHO.
+
+
+ECHO. & ECHO ******************************************************
+SET /p URL="Paste URL: "
+ECHO URL=!URL!;
+ECHO ****************************************************** & ECHO.
+
+REM _________________MAIN_____________________
+	youtube-dl.exe --config-location youtube-dl-Video-1080p-FPS30.conf !URL!
+REM _______________END_MAIN___________________
+
+ECHO.
+ECHO ******************************************************
+ECHO *        Downloads Are Ready :) Be Happy :)          *
+ECHO ******************************************************
+ECHO.
+
+pause
