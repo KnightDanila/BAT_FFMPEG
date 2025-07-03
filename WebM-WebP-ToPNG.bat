f283da9c2eb53811098981914dbcc48c57e428ae
KnightDanila <KnightDanila@i.ua>
2025-07-03 23:22:52 +0300

    added new scripts for converting WebM and WebP

---
+++ WebM-WebP-ToPNG.bat
@@ -0,0 +1,49 @@
+REM https://medium.com/@colten_jackson/doing-the-gif-thing-on-debian-82b9760a8483
+REM http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
+REM https://superuser.com/questions/556029/how-do-i-convert-a-video-to-gif-using-ffmpeg-with-reasonable-quality
+REM https://gist.github.com/keijiro/b46d94f4cd1b159c59a72cd616f95017
+REM https://superuser.com/questions/1231645/ffmpeg-generate-and-use-different-palette-for-every-frame
+
+
+@echo off
+
+echo %CD%
+echo %~dp0
+cd /d %~dp0
+ECHO. & ECHO ******* BAT FILE ******* 
+ECHO %~nx0 
+ECHO ************************ & ECHO.
+
+REM Creating the Results Directory - GIFs
+set RESULTS_DIR="IMGs"
+call SYS_MakeResultsDir.bat %RESULTS_DIR%
+
+REM For first file
+REM %1
+REM For multiple files
+echo %*
+
+set COUNTER=0
+for %%x in (%*) do (
+	echo %%x
+	set /A COUNTER+=1
+)
+echo COUNTER = %COUNTER%
+
+if %COUNTER% GTR 0 (
+	if %COUNTER% EQU 1 (
+		echo _____GET ONE FILE_____
+	) else (
+		echo _____GET MULTI FILES: %COUNTER% files_____
+	)
+	for %%x in (%*) do (
+REM //////////////////// MAIN \\\\\\\\\\\\\\\\\\\\\\\\\
+
+		ImageMagick_convert.exe %%x "%RESULTS_DIR%\%%~nx_WebpToPng.png"
+
+)
+) else (
+	echo _____GET NO ONE FILES_____
+)
+
+pause
